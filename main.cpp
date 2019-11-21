7fa019678fa0a63d48ae624818c9f2c8487331f1
Tatiana Dvorkina <tdvorkina@chihua.cab.spbu.ru>
2019-11-21 14:02:00 +0300

    Fix small issues

--- main.cpp
+++ main.cpp
@@ -156,13 +156,11 @@ private:
                 dp[0][j][0] = mismatch;
             }
             for (int k = 1; k < m.seq.size(); ++ k) {
-                dp[0][j][k] = dp[0][j][k-1] + del;
+                int mm_score = monomers_[j].seq[k] == read.seq[0] ? match: mismatch;
+                dp[0][j][k] = max(dp[0][j][k-1] + del, del*(k-1) + mm_score);
             }
         }
-        //dp[0][monomers_num][0] = 0;
-        //cout << "Init dp " << read.seq.size() << endl;
         for (int i = 1; i < read.seq.size(); ++ i) {
-            //dp[i][monomers_num][0] = dp[i-1][monomers_num][0];
             for (int j = 0; j < monomers_.size(); ++ j) {
                 dp[i][monomers_num][0] = max(dp[i][monomers_num][0], dp[i-1][j][monomers_[j].size() - 1]);
             }
@@ -188,8 +186,7 @@ private:
                 }
             }
         }
-        //cout << "Counted dp \n";
-        int max_score = INF; //dp[read.seq.size()-1][monomers_num][0];
+        int max_score = INF;
         int best_m = monomers_num;
         for (int j = 0; j < monomers_.size(); ++ j) {
             if (max_score < dp[read.seq.size()-1][j][monomers_[j].size() -1] ) {
@@ -197,7 +194,6 @@ private:
                 best_m = j;
             }
         }
-        //cout << max_score << endl;
         vector<MonomerAlignment> ans;
         int i = read.seq.size() - 1;
         int j = best_m;
@@ -276,18 +272,11 @@ private:
                 }
             }
             for (size_t j = i + 1; j < (size_t) min((int) i + 4, (int) batch.size()); ++ j) {
-                if ((batch[j].end_pos - batch[i].end_pos < 50 || batch[j].start_pos - batch[i].start_pos < 50) && batch[i].identity < batch[j].identity) {
+                if ((batch[j].end_pos - batch[i].end_pos < 50 || batch[j].start_pos - batch[i].start_pos < 50) && batch[i].identity <= batch[j].identity) {
                     add = false;
                 }
             }
             if (add) { res.push_back(batch[i]);}
-            // if (batch[i].monomer_name == batch[i - 1].monomer_name && batch[i].end_pos - batch[i-1].end_pos < 150) {
-            //     if (batch[i].identity > batch[i-1].identity) {
-            //         res[res.size() - 1] = batch[i];
-            //     }
-            // } else {
-            //     res.push_back(batch[i]);
-            // }
         }
         return res;
     }
