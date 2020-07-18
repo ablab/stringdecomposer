b487ec5b8a2103886d7486ca0675cf3eb1af2be5
seryrzu <seryrzu@gmail.com>
2020-07-17 22:28:48 -0700

    add basic utils

---
+++ sd/utils/bio.py
@@ -0,0 +1,56 @@
+# (c) 2020 by Authors
+# This file is a part of the SD program.
+# see LICENSE file
+
+from Bio import SeqIO
+import numpy as np
+import edlib
+from itertools import groupby
+import os
+import re
+import textwrap
+
+from joblib import Parallel, delayed
+
+from utils.various import weighted_random_by_dct
+
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+def read_bio_seq(filename):
+    seqs = read_bio_seqs(filename)
+    return str(list(seqs.values())[0])
+
+
+def read_bio_seqs(filename):
+    form = filename.split('.')[-1]
+    if form == 'fa' or form == 'fna':
+        form = 'fasta'
+    elif form == 'fq':
+        form = 'fastq'
+    seqs = SeqIO.parse(filename, format=form)
+    seqs = {seq.id: str(seq.seq) for seq in seqs}
+    return seqs
+
+
+trans = str.maketrans('ATGCatgc-', 'TACGtacg-')
+
+
+def RC(s):
+    return s.translate(trans)[::-1]
+
+
+def write_bio_seqs(filename, seqs, width=60):
+    with open(filename, 'w') as f:
+        for seq_id, seq in seqs.items():
+            print(f'>{seq_id}', file=f)
+            if width is None:
+                print(seq, file=f)
+                continue
+            start = 0
+            seq_len = len(seq)
+            while seq_len - start >= width:
+                print(seq[start:start+width], file=f)
+                start += width
+            print(seq[start:], file=f)
