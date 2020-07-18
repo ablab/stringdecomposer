ebeea1402533dacedb12fc1cc39109c593d2d595
seryrzu <seryrzu@gmail.com>
2020-07-18 12:30:26 -0700

    upd to monomer_db

--- sd/sd_parser/sd_parser.py
+++ sd/sd_parser/sd_parser.py
@@ -15,7 +15,7 @@ logger = logging.getLogger("SD.sd_parser.sd_parser")
 
 class SD_Report:
     def __init__(self, sd_report_fn, monomers_fn, sequences_fn,
-                 mode=None, hpc=True, cluster=True):
+                 mode=None, tocluster=False):
         logger.info('Reading SD Report')
 
         if mode is not None:
@@ -26,7 +26,8 @@ class SD_Report:
         logger.info(f'    sd_report_fn = {sd_report_fn}')
         logger.info(f'    monomers_fn  = {monomers_fn}')
         logger.info(f'    sequences_fn = {sequences_fn}')
-        monomer_db = MonomerDB.from_fasta_file(monomers_fn, cluster=cluster)
+        monomer_db = MonomerDB.from_fasta_file(monomers_fn,
+                                               tocluster=tocluster)
 
         logger.info('Reading SD Report from csv')
         report = pd.read_csv(sd_report_fn, sep='\t',
