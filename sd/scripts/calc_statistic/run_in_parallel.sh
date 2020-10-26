5e1271f0b4bf4da24ccec32708f7b2c59e68265c
Olga Kunyavskaya <OChe24912@gmail.com>
2020-10-26 10:11:54 +0300

    fix script for build matching heatmap

--- sd/scripts/calc_statistic/run_in_parallel.sh
+++ sd/scripts/calc_statistic/run_in_parallel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for MONOMER_ID in 1 2 3 .. 12
+for MONOMER_ID in 2 3 4 5 6 7 8 9 10 11 12
 do 
-    python3 build_matching_heatmap.py $MONOMAR_ID&
+    python3 build_matching_heatmap.py "$MONOMER_ID" &
+    echo "python3 buils_matchng_heatmap.py $MONOMER_ID &"
     echo "Script on $MONOMER_ID strted"
 done
 
