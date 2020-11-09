1d04d9e83ec59e9845775b751d3150ae9eb8db4f
Olga Kunyavskaya <Oche24912@yandex.ru>
2020-11-09 10:13:02 +0300

    calc dist after iteration to original monomers

--- sd/scripts/calc_statistic/reportMonomerGenerator.py
+++ sd/scripts/calc_statistic/reportMonomerGenerator.py
@@ -26,6 +26,24 @@ def load_fasta(filename, tp = "list"):
     return records
 
 
+def load_last_fasta(filename):
+    records = list(SeqIO.parse(filename, "fasta"))
+    records[-1] = records[-1].upper()
+    return records[-1]
+
+
+def get_min_dist(last_monomer, origin_monomers):
+    res_dist = 100
+    for omon in origin_monomers:
+        a = str(origin_monomers[omon].seq)
+        b = str(last_monomer.seq)
+        result = edlib.align(a, b, mode="NW", task="locations")
+        dist = result["editDistance"] * 100 / max(len(a), len(b))
+        res_dist = min(res_dist, dist)
+
+    return res_dist
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Compare Origin and Reported monomers')
     parser.add_argument('MGdir', help='path to dir with MonomerGenerator results')
@@ -51,6 +69,26 @@ def main():
                 result = edlib.align(a, b, mode="NW", task="path")
                 nice = edlib.getNiceAlignment(result, a, b)
                 writer.writerow([omon, emon, str(dist), "\n".join(nice.values())])
+
+    summary_path = os.path.join(args.MGdir, "summary.csv")
+    summary_upgrade = os.path.join(args.MGdir, "summary_upgrade.csv")
+    with open(summary_path, "r") as f:
+        reader = csv.reader(f)
+        with open(summary_upgrade, "w") as fw:
+            writer = csv.writer(fw)
+            line_id = 0
+            for line in reader:
+                if line_id == 0:
+                    writer.writerow(line + ["Dist to original monomers"])
+                else:
+                    nxt_folder_mono = os.path.join(args.MGdir, "iter_"  + str(line_id), "monomers.fa")
+                    if (os.path.isfile(nxt_folder_mono)):
+                        last_monomer = load_last_fasta(nxt_folder_mono)
+                        mdist = get_min_dist(last_monomer, origin_monomers)
+                        writer.writerow(line + [str(mdist)])
+                    else:
+                        writer.writerow(line + ["-"])
+                line_id += 1
 
 if __name__ == "__main__":
     main()
