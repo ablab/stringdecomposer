2d3dbd3f174f20d85c5d687847d020e46ac3c311
Tatiana Dvorkina <tdvorkina@chihua.cab.spbu.ru>
2021-01-15 21:02:44 +0300

    Fix HOR extraction procedure

--- sd/scripts/extract_hors.py
+++ sd/scripts/extract_hors.py
@@ -111,6 +111,15 @@ def build_full_hor(new_hor, hors, name):
     hors[name] = "_".join(new_hor_represent)
     return hors
 
+def get_annotation_seq_sz(annotation_seq):
+    prev = annotation_seq[0]
+    cnt = 1
+    for i in range(1, len(annotation_seq)):
+        if annotation_seq[i] != prev:
+            cnt += 1
+            prev = annotation_seq[i]
+    return cnt
+
 def known_hors_annotation(reads_annotation, known_hors, hors, hors_lst, h_cnt, min_cnt):
     hors_log = []
     rev_comp_hors = []
@@ -125,11 +134,11 @@ def known_hors_annotation(reads_annotation, known_hors, hors, hors_lst, h_cnt, m
             for a in annotation:
                 annotation_seq.append(a[0] + "[" + str(a[1]) + "]")
             annotation_str = "_".join(annotation_seq)
-            set_size += len(annotation_seq)
+            set_size += get_annotation_seq_sz(annotation_seq)
             annotation_new_lst = annotation_str.split(kh)
             annotation_new_str = annotation_str.replace(kh, "X")
-            annotation_new_str = annotation_new_str.replace("X_X", "X")
-            new_set_size += len(annotation_new_str.split("_"))
+            annotation_new_str = re.sub(r'(X_)\1+', r'\1', (annotation_new_str + "_"))[:-1]
+            new_set_size += get_annotation_seq_sz(annotation_new_str.split("_"))
             cnt += len(annotation_new_lst) - 1
         if cnt == 0:
             continue
@@ -146,8 +155,7 @@ def known_hors_annotation(reads_annotation, known_hors, hors, hors_lst, h_cnt, m
             annotation_seq = []
             for a in annotation:
                 annotation_seq.append(a[0] + "[" + str(a[1]) + "]")
-            annotation = update_annotation(annotation, annotation_seq, kh, name)
-            reads_annotation[r] = collapse_annotation(annotation)
+            reads_annotation[r] = update_annotation(annotation, annotation_seq, kh, name)
 
     return reads_annotation, hors, hors_lst, h_cnt, hors_log
 
@@ -161,6 +169,25 @@ def update_annotation(annotation, annotation_seq, new_hor, name):
         else:
             cur_seq = ""
         if cur_seq == new_hor:
+            cnt_sz = sum([annotation[j][2]["sz"] for j in range(i, i + k)])
+            new_annotation.append([name, 1, {"s": [annotation[i][2]["s"][0]], "e": [annotation[i + k - 1][2]["e"][-1]], "sz": cnt_sz }])
+            i += k
+        else:
+            new_annotation.append(annotation[i])
+            i += 1
+    return new_annotation
+
+def update_annotation_superhor(annotation, annotation_seq, new_hor, name):
+    new_annotation = []
+    k = len(new_hor.split("_"))
+    i = 0
+    while i < len(annotation):
+        if i + k < len(annotation):
+            cur_seq = "_".join(annotation_seq[i:i+k])
+        else:
+            cur_seq = ""
+        cur_seq_transformed = re.sub(r"\[\d+\]", "[1]", cur_seq)
+        if cur_seq_transformed == new_hor:
             new_annotation.append([name, 1, {"s": [annotation[i][2]["s"][0]], "e": [annotation[i + k - 1][2]["e"][-1]]} ])
             i += k
         else:
@@ -170,58 +197,61 @@ def update_annotation(annotation, annotation_seq, new_hor, name):
 
 def collapse_annotation(annotation):
     new_annotation = []
-    prev, cnt, start, end = "", 0, [], []
+    prev, cnt, cnt_sz, start, end = "", 0, 0, [], []
     for i in range(len(annotation)):
         c, c_cnt = annotation[i][:2]
         if c == prev:
             cnt += c_cnt
+            cnt_sz += annotation[i][2]["sz"]
             start.extend(annotation[i][2]["s"])
             end.extend(annotation[i][2]["e"])
         else:
             if prev != "":
-                new_annotation.append([prev, cnt, {"s": start, "e": end}])
+                new_annotation.append([prev, cnt, {"s": start, "e": end, "sz": cnt_sz}])
             prev = c 
             cnt = c_cnt
+            cnt_sz = annotation[i][2]["sz"]
             start = annotation[i][2]["s"]
             end = annotation[i][2]["e"]
     i = len(annotation) - 1
-    new_annotation.append([prev, cnt, {"s": start, "e": end}])
+    new_annotation.append([prev, cnt, {"s": start, "e": end, "sz": cnt_sz}])
     return new_annotation
 
 def find_potential_hors(annotation, annotation_seq, min_hor_len, max_hor_len, hors, potential_hors_all, potential_hors_names_all, set_size):
     potential_hors = {}
     potential_hors_names = []
     annotation_str = "_".join(annotation_seq)
+    cur_set_size = get_annotation_seq_sz(annotation_seq)
     for i in range(len(annotation)):
         end_ind = i
         len_subseq, subseq = 0, []
         len_monomer_subseq = 0
+        has_diff = False
         while end_ind < len(annotation) and len_subseq < max_hor_len \
               and annotation[end_ind][0] != "NM" and not annotation[end_ind][0].startswith("f") :
-            len_subseq += annotation[end_ind][1]
-            if annotation[end_ind][0] in hors:
-                len_monomer_subseq += len(hors[annotation[end_ind][0]].split("_"))*annotation[end_ind][1]
-            else:
-                len_monomer_subseq += annotation[end_ind][1]
+            len_subseq += annotation[end_ind][2]["sz"]
             subseq.append(annotation_seq[end_ind])
             end_ind += 1
-            if min_hor_len < len_monomer_subseq < max_hor_len:
-                subseq_str = "_".join(subseq)
+            if min_hor_len < len_subseq < max_hor_len:
+                if len(subseq) > 1 and subseq[-1] != subseq[-2]:
+                    has_diff = True
+                if has_diff:
+                    subseq_str = "_".join(subseq)
 
-                if "NM" in subseq_str or "f" in subseq_str:
-                    print(subseq_str)
-                    exit(-1)
-                if subseq_str not in potential_hors:
-                    annotation_new_lst = annotation_str.split(subseq_str)
-                    annotation_new_str = annotation_str.replace(subseq_str, "X")
-                    annotation_new_str = re.sub(r'(X_)\1+', r'\1', (annotation_new_str + "_"))[:-1]
+                    if "NM" in subseq_str or "f" in subseq_str:
+                        print(subseq_str)
+                        exit(-1)
+                    if subseq_str not in potential_hors:
+                        annotation_new_lst = annotation_str.split(subseq_str)
+                        annotation_new_str = annotation_str.replace(subseq_str, "X")
+                        annotation_new_str = re.sub(r'(X_)\1+', r'\1', (annotation_new_str + "_"))[:-1]
 
-                    new_set_size = len(annotation_new_str.split("_"))
-                    potential_hors[subseq_str] = {"set_size": new_set_size, "cnt": len(annotation_new_lst) - 1}
-                    potential_hors_names.append(subseq_str)
+                        new_set_size = get_annotation_seq_sz(annotation_new_str.split("_"))
+                        potential_hors[subseq_str] = {"set_size": new_set_size, "cnt": len(annotation_new_lst) - 1}
+                        potential_hors_names.append(subseq_str)
     for h in potential_hors_all:
         if h not in potential_hors:
-            potential_hors_all[h]["set_size"] += len(annotation)
+            potential_hors_all[h]["set_size"] += cur_set_size
         else:
             potential_hors_all[h]["set_size"] += potential_hors[h]["set_size"]
             potential_hors_all[h]["cnt"] += potential_hors[h]["cnt"]
@@ -234,14 +264,12 @@ def find_potential_hors(annotation, annotation_seq, min_hor_len, max_hor_len, ho
             potential_hors_all[h]["cnt"] = potential_hors[h]["cnt"]
     return potential_hors_all, potential_hors_names_all
 
-
-def run_iterative_hor_extraction(annotation, known_hors, min_cnt, min_weight, min_hor_len, max_hor_len):
+def run_iterative_hor_extraction(annotation, known_hors, min_cnt, min_weight, min_hor_len, max_hor_len, superhor = False):
     hors = {}
     hors_lst = []
     h_cnt = 0
     reads = []
     for r in annotation:
-        annotation[r] = collapse_annotation(annotation[r])
         reads.append(r)
     reads = sorted(reads)
 
@@ -256,7 +284,10 @@ def run_iterative_hor_extraction(annotation, known_hors, min_cnt, min_weight, mi
         for r in reads:
             annotation_seq = []
             for a in annotation[r]:
-                annotation_seq.append(a[0] + "[" + str(a[1]) + "]")
+                if superhor:
+                    annotation_seq.append(a[0] + "[1]")
+                else:
+                    annotation_seq.append(a[0] + "[" + str(a[1]) + "]")
             # if h_cnt == 0:
             #     print("_".join(annotation_seq).replace("[1]", ""))
             potential_hors, potential_hors_names = find_potential_hors(annotation[r], annotation_seq, min_hor_len, max_hor_len, hors, potential_hors, potential_hors_names, set_size)
@@ -280,8 +311,12 @@ def run_iterative_hor_extraction(annotation, known_hors, min_cnt, min_weight, mi
             annotation_seq = []
             for a in annotation[r]:
                 annotation_seq.append(a[0] + "[" + str(a[1]) + "]")
-            annotation[r] = update_annotation(annotation[r], annotation_seq, potential_hors_lst[0][0], name)
-            annotation[r] = collapse_annotation(annotation[r])
+            if superhor:
+                annotation[r] = update_annotation_superhor(annotation[r], annotation_seq, potential_hors_lst[0][0], name)
+            else:
+                annotation[r] = update_annotation(annotation[r], annotation_seq, potential_hors_lst[0][0], name)
+    for r in reads:
+        annotation[r] = collapse_annotation(annotation[r])
     return annotation, hors_lst, hors_log
 
 def build_graph(known_hors):
@@ -313,7 +348,7 @@ def run_naive_hor_annotation(annotation, known_hors):
                 new_annotation[r].append(annotation[r][i])
             elif (prev_m in kh_graph and cur_m in kh_graph[prev_m]) or (prev_m in kh_graph_rev and cur_m in kh_graph_rev[prev_m]):
                 rec = new_annotation[r][-1]
-                new_annotation[r][-1] = [rec[0] + "_" + cur_m, 1, {"s": [rec[2]["s"][0]], "e": [coords["e"][-1]] }]
+                new_annotation[r][-1] = [rec[0] + "_" + cur_m, 1, {"s": [rec[2]["s"][0]], "e": [coords["e"][-1]], "sz": len(rec[0].split("_")) + 1}]
             prev_m, prev_cnt, prev_coords = cur_m, cur_cnt, coords
 
     hors_mp = {}
@@ -326,7 +361,7 @@ def run_naive_hor_annotation(annotation, known_hors):
                     hors_mp[new_annotation[r][i][0]] = "h" + str(cnt + 1)
                     cnt += 1
                 new_annotation[r][i][0] = hors_mp[new_annotation[r][i][0]]
-        new_annotation[r] = collapse_annotation(new_annotation[r])
+        #new_annotation[r] = collapse_annotation(new_annotation[r])
     return new_annotation, hors_lst, hors_log
 
 def form_hor_dec(annotation, seq):
@@ -364,6 +399,76 @@ def convert_to_list_of_monomers(monomers_lst, monomers_mp):
         res.append(m_name)
     return ",".join(res)
 
+def convert_to_ivan_hors(monomers_lst, monomers_mp, known_hors_initial):
+    res = []
+    for m in monomers_lst:
+        m_name = convert_to_initial_mono(m, monomers_mp)
+        res.append(m_name)
+    isknown, isknown_rev = False, ""
+
+    if ",".join(res) in known_hors_initial:
+        isknown = True
+    if ",".join(res[::-1]).replace("'", "") in known_hors_initial:
+        isknown = True
+        isknown_rev = "'"
+    name = []
+    prev_hor, prev_num, prev_rev = None, -1, False
+    for m in res:
+        if m.startswith("S"):
+            hor, num = m.split(".")
+            rev = False
+            if num.endswith("'"):
+                rev = True
+                num = num[:-1]
+            if prev_hor != hor:
+                name.append(hor)
+                if rev:
+                    name.append(num + "-" + num + "'")
+                else:
+                    name.append(num + "-" + num)
+            else:
+                if "/" not in num and prev_rev==rev==False and prev_num + 1 == int(num):
+                    name[-1] = name[-1].split("-")[0] + "-" + num
+                elif "/" not in num and prev_rev==rev==True and prev_num - 1 == int(num):
+                    name[-1] = name[-1].split("-")[0] + "-" + num + "'"
+                elif rev:
+                    name.append(num + "-" + num + "'")
+                else:
+                    name.append(num + "-" + num)
+            if "/" in num:
+                prev_hor, prev_num, prev_rev = hor, -100, rev
+            else:
+                prev_hor, prev_num, prev_rev = hor, int(num), rev
+        else:
+            name.append(m)
+            prev_hor, prev_num, prev_rev = None, -1, False
+    new_name = []
+    for m in name:
+        add = ""
+        if m.endswith("'"):
+            m = m[:-1]
+            add = "'"
+        if len(m.split("-")) > 1 and m.split("-")[0] == m.split("-")[1]:
+            new_name.append(m.split("-")[0] + add)
+        else:
+            new_name.append(m + add)
+    collapsed_name = []
+    prev = None
+    for m in new_name:
+        if len(collapsed_name) == 0 or prev != m:
+            collapsed_name.append(m)
+        else:
+            m_name = collapsed_name[-1].split("[")[0]
+            num = 1
+            if len(collapsed_name[-1].split("[")) > 1:
+                num = int(collapsed_name[-1].split("[")[1].split("]")[0])
+            collapsed_name[-1] = m_name + "[" + str(num + 1) + "]"
+        prev = m
+    if isknown:
+        return collapsed_name[0] + isknown_rev
+    else:
+        return "_".join(collapsed_name)
+
 def print_hor_dec(filename, seq, monomers_mp, known_hors_initial):
     prev, prev_qid, start = 0, "", 0
     with open(filename, "w") as fout:
@@ -379,7 +484,7 @@ def print_hor_dec(filename, seq, monomers_mp, known_hors_initial):
                     if c["qid"] in monomers_mp:
                         fout.write("\t".join([r, convert_to_initial_mono(c["qid"], monomers_mp), str(c["len"]) if not c["qid"].startswith("f") else "-1", "{0:.2f}".format(c["idnt"]), str(c["s"]), str(c["e"]), str(c["e"] - c["s"] + 1), str(c["s"] - prev)]) + "\n")
                     else:
-                        fout.write("\t".join([r, convert_to_list_of_monomers(c["monomers_lst"], monomers_mp), str(c["len"]), "{0:.2f}".format(c["idnt"]), str(c["s"]), str(c["e"]), str(c["e"] - c["s"] + 1),  str(c["s"] - prev)]) + "\n")
+                        fout.write("\t".join([r, convert_to_ivan_hors(c["monomers_lst"], monomers_mp, known_hors_initial), str(c["len"]), "{0:.2f}".format(c["idnt"]), str(c["s"]), str(c["e"]), str(c["e"] - c["s"] + 1),  str(c["s"] - prev)]) + "\n")
                 prev = c["e"]
                 prev_qid = c["qid"]
         if prev_qid == "NM":
@@ -387,7 +492,7 @@ def print_hor_dec(filename, seq, monomers_mp, known_hors_initial):
     print("Saved to ", filename)
 
 
-def build_hor_annotation(reads_dec, min_cnt, min_weight, min_len, max_len, monomers_mp, monomers_mp_r, output, canonical, naive_dec):
+def build_hor_annotation(reads_dec, min_cnt, min_weight, min_len, max_len, monomers_mp, monomers_mp_r, output, canonical, naive_dec, run_superhor):
     annotation = {}
     seq = {}
     for read in reads_dec:
@@ -395,8 +500,8 @@ def build_hor_annotation(reads_dec, min_cnt, min_weight, min_len, max_len, monom
         seq[read] = []
         annotation[read] = []
         for i in range(len(dec)):
-            annotation[read].append([dec[i]["qid"], 1, {"s": [dec[i]["s"]], "e": [dec[i]["e"]], "idnt": dec[i]["idnt"]}])
-            seq[read].append({"qid": dec[i]["qid"], "len": 1, "s": dec[i]["s"], "e": dec[i]["e"], "idnt": dec[i]["idnt"]})
+            annotation[read].append([dec[i]["qid"], 1, {"s": [dec[i]["s"]], "e": [dec[i]["e"]], "idnt": dec[i]["idnt"], "sz": 1}])
+            seq[read].append({"qid": dec[i]["qid"], "len": 1, "s": dec[i]["s"], "e": dec[i]["e"], "idnt": dec[i]["idnt"], "sz": 1})
 
     known_hors = []
     known_hors_initial = set()
@@ -408,6 +513,9 @@ def build_hor_annotation(reads_dec, min_cnt, min_weight, min_len, max_len, monom
         annotation, hors_lst, hors_log = run_naive_hor_annotation(annotation, known_hors)
     else:
         annotation, hors_lst, hors_log = run_iterative_hor_extraction(annotation, known_hors, min_cnt, min_weight, min_len, max_len)
+
+    # if run_superhor:
+    #     annotation, hors_lst, hors_log = run_iterative_hor_extraction(annotation, [], min_cnt, min_weight, min_len, max_len, True)
 
     for r in annotation:
         annotation_seq = []
@@ -430,6 +538,7 @@ if __name__ == "__main__":
     parser.add_argument('output', help='tsv-file to save HOR decomposition')
     parser.add_argument('--canonical', help='txt-file with list of canonical HORs', required = False)
     parser.add_argument('--naive',  help='run naive decomposition using canonical HORs (divides into canonical HORs and their subsequences, --canonical is required)', action="store_true")
+    #parser.add_argument('--superhor',  help='run decomposition into superHORs after naive or classic HOR decomposition', action="store_true")
     parser.add_argument('--min-idnt',  help='minimum identity of monomer (75 by default)', type=int, default=75, required = False)
     parser.add_argument('--min-reliable',  help='minimum identity of reliable monomer (95, by default)', type=int, default=95, required = False)
     parser.add_argument('--min-cnt',  help='minimum number of potential HOR occurrences to be considered (5 by default)', type=int, default=5, required = False)
@@ -447,6 +556,6 @@ if __name__ == "__main__":
     dec, monomers_mp, monomers_mp_r = convert_dec_to_internal_monomers(load_dec(args.decomposition, args.min_idnt, args.min_reliable), monomers)
     filename = args.output
 
-    build_hor_annotation(dec, args.min_cnt, args.min_weight, args.min_len, args.max_len, monomers_mp, monomers_mp_r, args.output, args.canonical, args.naive)
+    build_hor_annotation(dec, args.min_cnt, args.min_weight, args.min_len, args.max_len, monomers_mp, monomers_mp_r, args.output, args.canonical, args.naive, args.superhor)
 
 
