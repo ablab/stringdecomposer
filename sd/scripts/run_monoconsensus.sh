5ed1b3ff33c5bf13df30da7d74df87786cfd8f0c
Tatiana Dvorkina <tdvorkina@chihua.cab.spbu.ru>
2021-06-08 19:24:50 +0300

    Add script to generate monomer consensuses from CenSat

--- sd/scripts/run_monoconsensus.sh
+++ sd/scripts/run_monoconsensus.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 #array=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X)
-#array=(2 3 4 6 7 8 9 10 11 12 13 14 15 16 17 18 20 21 22 X)
-array=( X )
+array=(X)
 
 for i in "${array[@]}"
 do
    echo ${i}
-   array2=(/Sid/tdvorkina/monomers/CenSat/IvanAnnotByHOR/chr${i}*.bed)
+   array2=(/Sid/tdvorkina/monomers/CenSat/IvanAnnotByHOR/chr${i}_*.bed)
    for f in "${array2[@]}"
    do
-       echo $f
-       python3 build_monoconsensus.py /Sid/tdvorkina/monomers/CenSat/Asm/chr${i}.fasta $f /Sid/tdvorkina/monomers/CenSat/HORdec/cen${i} --hors /Sid/tdvorkina/monomers/CenSat/IvanHORs/HOR${i}.tsv
+       echo "$(basename -- $f .bed)"
+       python3 build_monoconsensus.py /Sid/tdvorkina/monomers/CenSat/Asm/chr${i}.fasta $f /Sid/tdvorkina/monomers/CenSat/HORdec/"$(basename -- $f .bed)" --hors /Sid/tdvorkina/monomers/CenSat/IvanHORs/HOR${i}.tsv
    done
 done
