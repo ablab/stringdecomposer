88564aff5deb2b598d141deb4c3f3de0eb174e32
seryrzu <seryrzu@gmail.com>
2021-06-24 23:19:39 -0700

    actually commit git and logger

---
+++ py/git.py
@@ -0,0 +1,13 @@
+# (c) 2020 by Authors
+# This file is a part of centroFlye program.
+# Released under the BSD license (see LICENSE file)
+
+import subprocess
+
+
+def get_git_revision_hash():
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+
+
+def get_git_revision_short_hash():
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
